9ca0e87cfe66f7501a4ca15998249f186ce79a6e
Fatimatul Zaharah <fatimatul@supplycart.my>
2022-02-18 19:54:22 +0800

    add option city list

--- components/CurrentWeather.vue
+++ components/CurrentWeather.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-gray-100 h-screen">
       <div class="static p-2 border bg-black border-b shadow flex justify-between header">
           <div class="flex w-2/3 mx-auto justify-between items-center">
             <div class="flex space-x-2 items-center">
@@ -7,27 +7,48 @@
                 <h1 v-if="data" class="text-white ml-2">
                     {{data.name}}, {{currentCountry}} {{currentTemperatureRounded}}°<span style="font-size:0.5rem">C</span>
                 </h1>
-                 <img :src="`http://openweathermap.org/img/wn/${weatherIcon}.png`" height="27" width="27"/>
+                <img :src="`http://openweathermap.org/img/wn/${weatherIcon}.png`" height="27" width="27"/>
             </div>
          
             <div class="flex items-center space-x-2">
-                <div class="flex items-center px-2 bg-white rounded rounded-md w-80" style="height:34px">
-                    <div class="w-full grid grid-cols-3 divide-x">
-                        <div class="col-span-2 flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                            <input type="text" placeholder="Search" class="focus:ring-0 focus:outline-none placeholder:italic placeholder:text-slate-400 placeholder:text-xs" />
-                        </div>
-                        <div @click="isOpen = !isOpen" class="flex items-center cursor-pointer space-x-2 justify-center location-btn" :class="{'open':isOpen}">
-                            <div class="text-sm">Location</div>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 9l-7 7-7-7" />
-                            </svg>
+                <div class="btn-dropdown inline-block relative">
+                    <div class="flex items-center px-2 bg-white rounded rounded-md w-80" style="height:34px">
+                        <div class="w-full grid grid-cols-3 divide-x">
+                            <div class="col-span-2 flex">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
+                                <input @focus="searching = true" type="text" placeholder="Search" class="focus:ring-0 focus:outline-none placeholder:italic placeholder:text-slate-400 placeholder:text-xs" />
+                            </div>
+                            <div @click="isOpen = !isOpen" class="flex items-center cursor-pointer space-x-2 justify-center location-btn" :class="{'open':isOpen}">
+                                <div class="text-sm">Location</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </div>
+
                         </div>
 
                     </div>
-
+                    <div v-if="searching"
+                        class="dropwdown-item right w-80 rounded-b-md shadow border text-gray-700 text-sm"
+                    >
+                            <div class="p-2 flex space-x-2 border-b sticky" @click="selectCurrentLocation()">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                <span>
+                                    Current Location
+                                </span>
+                            </div>
+                            <div class="city-options">
+                                <div v-for="(item, index) in cities" :key="index" class="p-2 item" @click="selectCity(item)">
+                                    <span>{{item.city}}</span>
+                                </div>
+                            </div>
+                        
+                    </div>
                 </div>
                 <div class="text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -39,53 +60,34 @@
 
       </div>
       {{data}}
+      <WeatherCard  />
   </div>
 </template>
 
 <script>
-import {COUNTRIES} from "../constants/countries"
+import WeatherMixin from "../mixins/weather"
+import {CITIES_COORDS} from "../constants/malaysiaCitiesCoord"
 export default {
+    mixins: [WeatherMixin],
     data() {
         return {
-            data: null,
-            isOpen:false,
-            COUNTRIES
+            cities:CITIES_COORDS
         }
-    },
-    computed: {
-        currentTemperatureRounded() {
-            return this.data ? Math.round(this.data.main.temp) : null
-        },
-        currentCountry() {
-            if(this.data) {
-                const currCountry = this.COUNTRIES.find((country) => country.code == this.data.sys.country)
-                return currCountry.name
-            }
-            return 'Malaysia'
-        },
-        weatherIcon() {
-            return this.data?.weather[0].icon ?? '02n'
-        }
-    },
-    mounted() {
-        this.getData()
     },
     methods: {
-        async getData() {
-            navigator.geolocation.getCurrentPosition((pos)=> {
-                var crd = pos.coords;
-
-                this.getWeather(crd)
-            })
-
+        selectCurrentLocation() {
+            this.getData()
+            this.searching = false
         },
-        async getWeather(coords) {
-            const response = await this.$axios.$get(`http://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=5f3819b22c7b564e4e62a670f1102556&units=metric`)
-            this.data = response
+        selectCity(city) {
+            const params = {
+                latitude:city.lat,
+                longitude:city.lng
+            }
+            this.getWeather(params)
+            this.searching = false
         }
-
     }
-
 }
 </script>
 
@@ -103,6 +105,26 @@ export default {
 .location-btn.open ~ svg{
     transition: all 0.3s ease;
     transform: rotate(180deg);
+}
+
+.btn-dropdown .dropwdown-item {
+    cursor: pointer;
+    background: white;
+    left:0;
+    position: absolute;
+    white-space: nowrap;
+    z-index: 2;
+    
+}
+
+.btn-dropdown .dropwdown-item .item:hover {
+    --tw-bg-opacity: 1;
+    background-color: rgb(229 231 235 / var(--tw-bg-opacity));
+}
+
+.city-options{
+    max-height: 200px;
+    overflow-y: auto;
 }
 
 </style>
---
+++ components/WeatherCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4 rounded-md shadow w-1/2 mx-auto bg-white">
+      <!-- {{data}} -->
+      <img :src="`http://openweathermap.org/img/wn/${weatherIcon}.png`" />
+  </div>
+</template>
+
+<script>
+import WeatherMixin from "../mixins/weather"
+export default {
+    mixins: [WeatherMixin],
+}
+</script>
+
+<style>
+
+</style>
